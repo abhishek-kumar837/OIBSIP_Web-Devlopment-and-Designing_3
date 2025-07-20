48598a89f580df12d970ec171cfc25b1cb1a5423
abhishek-kumar837 <abhisheksahu4115@gmail.com>
2025-07-20 09:18:43 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const inputBox = document.getElementById('inputBox');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+
+let editTodo = null;
+
+// Function to add todo
+const addTodo = () => {
+    const inputText = inputBox.value.trim();
